634673c2f6e87a0d2626ee5b6d3892ef0f3ea2fe
ysandeepjoshi <ysandeepjoshi@gmail.com>
2021-03-27 22:55:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//set up DB connection
+//set up DB connection..
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri,{useNewUrlParser:true,useCreateIndex:true})
 const connection = mongoose.connection;
